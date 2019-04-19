e6c3b83813bff4fb1b73384518d81acd357a32bb
wolski <wewolski@gmail.com>
2019-04-19 16:16:55 +0200

    compute_anova_lm added to deprecation

--- R/deprecated_auto_contrasts.R
+++ R/deprecated_auto_contrasts.R
@@ -109,3 +109,59 @@ deprecated_model_contrasts_no_interaction <- function(modelProteinF,
 }
 
 
+#' Perform anova analysis
+#' @export
+#' @importFrom glue glue
+#' @importFrom purrr map
+#' @examples
+#'
+#' library(LFQService)
+#' library(tidyverse)
+#' library(glue)
+#' config <- spectronautDIAData250_config$clone(deep=TRUE)
+#' config$parameter$min_nr_of_notNA  <- 20
+#' data <- spectronautDIAData250_analysis
+#' data <- transform_work_intensity(data, config, log2)
+#' x1 <- compute_anova_lm(data, config, hierarchy_level= 2, factor_level=1)
+#' x2 <- compute_anova_lm(data, config, hierarchy_level = 1, factor_level=2)
+#' x3 <- compute_anova_lm(data, config, hierarchy_level= 2, factor_level=2)
+#' (head(x1))
+deprecated_compute_anova_lm <- function(data, config, .formula=NULL, hierarchy_level=1, factor_level=1){
+  aovmodelfit <- function(x, formula){
+    tryCatch(anova(lm(formula , data=x)), error = function(e) return(NULL))
+  }
+
+  if(is.null(.formula)){
+    formulastr <- paste(config$table$getWorkIntensity(), " ~ ", paste(c(config$table$factorKeys()[1:factor_level],
+                                                                        config$table$hierarchyKeys(TRUE)[1],
+                                                                        config$table$fileName), collapse=" + "))
+    print(formulastr)
+    formula <- as.formula(formulastr)
+  }
+  message("formula :" , deparse(formula))
+  groupVars <-config$table$hierarchyKeys()[1:hierarchy_level]
+
+  pepRes <- data %>% dplyr::group_by(!!!syms(groupVars)) %>% tidyr::nest()
+  pepRes1 <- pepRes %>% dplyr::mutate(anova = map( data, aovmodelfit, formula))
+  head(pepRes1)
+  pepRes2 <- pepRes1 %>% dplyr::mutate(broomres = map(anova, broom::tidy))
+  pepRes3 <- pepRes2 %>%
+    dplyr::select(!!!syms(c(groupVars, "broomres"))) %>%
+    tidyr::unnest() %>%
+    dplyr::filter(term != "Residuals")
+
+  pVals <- pepRes3 %>% dplyr::select(!!!syms(c(groupVars,"term","p.value")))
+
+  pVals <- pVals %>% dplyr::mutate(term = glue("{term}.p.value"))
+  pVals <- pVals %>% tidyr::spread("term", "p.value")
+  df <- pepRes3 %>% dplyr::select(!!!syms(c(groupVars,"term","df")))
+  df <- df %>% dplyr::mutate(term = glue("{term}.df"))
+  df <- df %>% tidyr::spread(term, df)
+  statistic <- pepRes3 %>% dplyr::select(!!!syms(c(groupVars,"term","statistic")))
+  statistic <- statistic %>% dplyr::mutate(term = glue("{term}.statistic"))
+  statistic <- statistic %>% tidyr::spread(term, statistic)
+  res <- dplyr::inner_join(dplyr::inner_join(pVals, df, by=groupVars), statistic, by=groupVars)
+  return(res)
+}
+
+
