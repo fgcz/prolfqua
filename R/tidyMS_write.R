cda121864f28b4ccd7752d5fc276a4a643d96f05
Witold Wolski <wewolski@gmail.com>
2019-07-14 17:34:23 +0200

    fix

--- R/tidyMS_write.R
+++ R/tidyMS_write.R
@@ -8,17 +8,24 @@
 #' lfq_write_table(x, "test.csv")
 #' assign("lfq_write_format", "csv", envir = .GlobalEnv
 #' lfq_write_table(x, "test.csv")
+#'
+#' assign("lfq_write_format", "both", envir = .GlobalEnv
+#' lfq_write_table(x, "test.csv")
 lfq_write_table <- function(x, path){
   #lfq_write_format <- match.arg(lfq_write_format)
   if(!exists("lfq_write_format")){
     assign("lfq_write_format", "xlsx", envir = .GlobalEnv)
   }
-  print(lfq_write_format)
-  if(lfq_write_format == "csv"){
+  message("lfq_write_format", lfq_write_format)
+  if(lfq_write_format == "csv" | lfq_write_format == "both"){
+    message("writing csv")
     path <- paste0(tools::file_path_sans_ext(path),".csv")
     readr::write_csv(x, path = path)
-  }else if(lfq_write_format == "xlsx"){
-    path <- paste0(tools::file_path_sans_ext(path),".xslx")
+  }
+
+  if(lfq_write_format == "xlsx" | lfq_write_format == "both"){
+    message("writing xlsx")
+    path <- paste0(tools::file_path_sans_ext(path),".xlsx")
     writexl::write_xlsx(x, path = path)
   }
 }
