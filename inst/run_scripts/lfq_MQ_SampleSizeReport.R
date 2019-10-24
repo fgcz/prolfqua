85a527c33ca9b214b6b9409c3cb420f7b8757781
Witold Wolski <wewolski@gmail.com>
2019-10-24 14:47:59 +0200

    back to pdf knitr::kable fails

--- inst/run_scripts/lfq_MQ_SampleSizeReport.R
+++ inst/run_scripts/lfq_MQ_SampleSizeReport.R
@@ -100,7 +100,7 @@ LFQService::render_MQSummary_rmd(resDataStart,
                                  pep = TRUE,
                                  dest_path = outputDir,
                                  dest_file_name = paste0("r_",filename,"_Peptide"),
-                                 workdir = "." )
+                                 workdir = ".",format = "pdf" )
 
 
 peptideStats <- summarize_cv_raw_transformed(resDataStart, config)
@@ -136,7 +136,7 @@ LFQService::render_MQSummary_rmd(data,
                                  pep = FALSE,
                                  dest_path = outputDir,
                                  dest_file_name = paste0("r_",filename,"_Protein"),
-                                 workdir = ".")
+                                 workdir = ".", format="pdf")
 
 #rmarkdown::render("MQSummary2.Rmd", params=list(data =xx$data, configuration = xx$config), output_file = paste0("Protein_",mqzip,".pdf"))#,envir =new.env())
 #break()
