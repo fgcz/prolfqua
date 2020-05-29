6566ce041c3240aa6027283ee96f0cae92f2aa69
wolski <wewolski@gmail.com>
2020-05-29 08:32:23 +0200

    lint

--- inst/run_scripts/lfq_MQ_SampleSizeReport.R
+++ inst/run_scripts/lfq_MQ_SampleSizeReport.R
@@ -5,8 +5,9 @@ suppressMessages(library(tidyverse))
 suppressMessages(library(LFQService))
 suppressMessages(library(tidyr))
 suppressMessages(library(dplyr))
+library(docopt)
 
-"Sample Size Report from MQ file
+doc <- "Sample Size Report from MQ file
 
 Usage:
   lfq_MQ_SampleSizeReport.R <mqzip> [--outdir=<outdir>] [--project_Id=<project_Id>] [--order_Id=<order_Id>] [--workunit=<workunit>]
@@ -19,9 +20,8 @@ Options:
 
 Arguments:
   mqzip input file
-" -> doc
+"
 
-library(docopt)
 opt <- docopt(doc)
 
 cat("\nParameters used:\n\t",
@@ -32,22 +32,20 @@ cat("\nParameters used:\n\t",
     "  workunit:", workunit <- opt[["--workunit"]], "\n\n\n")
 
 
-if(!dir.exists(outputDir)){
-  if(!dir.create(outputDir)){
+
+if (!dir.exists(outputDir)) {
+  if (!dir.create(outputDir)) {
     stop("\n could not create : ", outputDir, "\n")
   }
 }
 
-assign("lfq_write_format", "xlsx", envir = .GlobalEnv)
-
-
 summarize_cv_raw_transformed <- function(resDataStart, config){
-  config_tmp <- config$clone(deep=TRUE)
+  config_tmp <- config$clone(deep = TRUE)
   stats_res <- summarize_cv(resDataStart, config_tmp)
   wide <- toWideConfig( resDataStart,config_tmp)
   stats_raw <- inner_join(stats_res, wide$data, by=config_tmp$table$hierarchyKeys())
 
-  resDataStart<- transform_work_intensity(resDataStart, config_tmp, log2)
+  resDataStart <- transform_work_intensity(resDataStart, config_tmp, log2)
   data <- LFQService::applyToIntensityMatrix(resDataStart, config_tmp, .func = robust_scale)
 
   stats_res_transformed <- summarize_cv(data, config_tmp)
@@ -96,48 +94,72 @@ resDataStart <- remove_small_intensities(resDataStart, config) %>%
 
 
 filename <- basename(tools::file_path_sans_ext(mqzip))
-LFQService::render_MQSummary_rmd(resDataStart,
-                                 config$clone(deep=TRUE),
-                                 pep = TRUE,
-                                 dest_path = outputDir,
-                                 dest_file_name = paste0("r_",filename,"_Peptide"),
-                                 workdir = ".",format = "html" )
+
+
+LFQService::render_MQSummary_rmd(
+  resDataStart,
+  config$clone(deep = TRUE),
+  pep = TRUE,
+  dest_path = outputDir,
+  dest_file_name = paste0("r_", filename, "_Peptide"),
+  workdir = ".",
+  format = "html"
+)
+
 
 
 peptideStats <- summarize_cv_raw_transformed(resDataStart, config)
-lfq_write_table(separate_hierarchy(peptideStats, config), path=file.path(outputDir,paste0("r_",filename,"_PeptideStats.csv")))
+lfq_write_table(separate_hierarchy(peptideStats, config),
+                path = file.path(outputDir, paste0("r_", filename, "_PeptideStats.csv")))
 
 
 # Perform filtering and Protein Aggregation...
 
+
 path <- ""
-results <- LFQService::workflow_MQ_protoV1(resDataStart,
-                                           config,
-                                           path,
-                                           peptideFilterFunction = LFQService:::.filter_proteins_by_peptide_count )
+res <- filter_proteins_by_peptide_count(resDataStart, config)
+results <- normalize_log2_robscale(res$data, res$config)
 
 
 #rmarkdown::render("MQSummary2.Rmd", params=list(configuration = config$clone(deep=TRUE), data = resDataStart), output_format = bookdown::pdf_document2())
-protintensity <- medpolish_protein_quants( results$pepIntensityNormalized, results$config_pepIntensityNormalized )
+protintensity <- medpolish_protein_quants( results$data, results$config )
 xx <- protintensity("unnest")
 data <- LFQService::applyToIntensityMatrix(xx$data, xx$config, .func = robust_scale)
-stats_res <- summarize_cv(data, xx$config, all=FALSE)
-data_wide <- inner_join(stats_res,toWideConfig(data, xx$config)$data, by = xx$config$table$hkeysDepth(), suffix =c(".factor",""))
+stats_res <- summarize_cv(data, xx$config, all = FALSE)
+data_wide <-
+  inner_join(
+    stats_res,
+    toWideConfig(data, xx$config)$data,
+    by = xx$config$table$hkeysDepth(),
+    suffix = c(".factor", "")
+  )
 
 
-lfq_write_table(separate_hierarchy(data_wide, config), path=file.path(outputDir, paste0("r_",filename,"_Protein.csv")))
-lfq_write_table(toWideConfig(data, xx$config)$annotation, path=file.path(outputDir, paste0("r_",filename,"_annotation.csv")))
+lfq_write_table(
+  separate_hierarchy(data_wide, config),
+  path = file.path(outputDir, paste0("r_", filename, "_Protein.csv")),
+  lfq_write_format = "xlsx"
+)
+lfq_write_table(
+  toWideConfig(data, xx$config)$annotation,
+  path = file.path(outputDir, paste0("r_", filename, "_annotation.csv")),
+  lfq_write_format = "xlsx"
+)
 
 ##```{r corrplot, fig.cap="Correlation plot."}
 ##LFQService::plot_sample_correlation(dataTransformed, config)
 ##```
 
-LFQService::render_MQSummary_rmd(data,
-                                 xx$config$clone(deep=TRUE),
-                                 pep = FALSE,
-                                 dest_path = outputDir,
-                                 dest_file_name = paste0("r_",filename,"_Protein"),
-                                 workdir = ".", format="html")
+
+LFQService::render_MQSummary_rmd(
+  data,
+  xx$config$clone(deep = TRUE),
+  pep = FALSE,
+  dest_path = outputDir,
+  dest_file_name = paste0("r_", filename, "_Protein"),
+  workdir = ".",
+  format = "html"
+)
 
 #rmarkdown::render("MQSummary2.Rmd", params=list(data =xx$data, configuration = xx$config), output_file = paste0("Protein_",mqzip,".pdf"))#,envir =new.env())
 #break()
