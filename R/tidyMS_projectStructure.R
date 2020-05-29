f4ef66e4c5c48f20e49bb9c60e011357c63ededc
wolski <wewolski@gmail.com>
2020-05-29 07:42:08 +0200

    Manage output structure

---
+++ R/tidyMS_projectStructure.R
@@ -0,0 +1,51 @@
+ProjectStructure <-
+  R6::R6Class("ProjectStructure", public = list(
+    outpath = "",
+    qc_path = "",
+    modelling_path = "",
+    initialize = function(outpath ,
+                          qc_path = "qc_results",
+                          modelling_path = "modelling_results"){
+      self$outpath = outpath
+      self$qc_path = file.path(outpath, qc_path )
+      self$modelling_path = file.path(outpath, modelling_path )
+    },
+    create_outpath = function(){
+      if (!dir.exists(self$outpath)) {
+        dir.create(self$outpath)
+      }
+    },
+    creat_qc_dir = function(){
+      self$create_outpath()
+      if (!dir.exists(self$qc_path)) {
+        dir.create(self$qc_path)
+      }
+    },
+    create_modelling_path = function(){
+      self$create_outpath()
+      if (!dir.exists(self$modelling_path)) {
+        dir.create(self$modelling_path)
+      }
+    },
+    create = function(){
+      self$creat_qc_dir()
+      self$create_modelling_path()
+    }
+
+  )
+  )
+
+#' Create Project structure
+#' @export
+#' @examples
+#' tmp <- createProjectStructure("./test_project")
+#' tmp$qc_path
+#' tmp$modelling_path
+#' if(FALSE){tmp$create()}
+#'
+createProjectStructure <- function(outpath,
+                                   qc_path = "qc_results",
+                                   modelling_path = "modelling_path"){
+  res <- ProjectStructure$new(outpath, qc_path, modelling_path)
+  return(res)
+}
