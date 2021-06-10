a6b2dc0e62455392ab75dfaee091967c96da9786
wolski <wewolski@gmail.com>
2021-06-10 09:53:13 +0200

    NOP changes in run2GrpAnalysis Application

--- inst/run_scripts/run2GrpAnalysis.R
+++ inst/run_scripts/run2GrpAnalysis.R
@@ -4,44 +4,40 @@ library(tidyverse)
 
 ################### CREATE SOME annotations
 datadir <- file.path(find.package("prolfquaData") , "quantdata")
+
 inputMQfile <-  file.path(datadir, "MAXQuant_ComboCourse_p2370_March_2017_WU183008.zip")
 inputAnnotation <- file.path(datadir, "annotation_ComboCourse_p2370_March_2017_WU183008.xlsx")
 
 GRP2 <- list()
-GRP2$projectID <- 3000
-GRP2$projectName <- "bbbbbbbbbbbbbbbbbbbb"
-GRP2$workunitID <- "WU444XDD"
-GRP2$nrPeptides <- 2
-
 
 factorDisplayName <- "Condition_"
 factorAnnotationName <- "condition"
+Contrasts <- c("GvsE" = "Condition_Glucose - Condition_Ethanol")
+
+GRP2$projectID <- 3000
+GRP2$projectName <- "bbbbbbbbbbbbbbbbbbbb"
+GRP2$workunitID <- "PDrun"
+
+GRP2$nrPeptides <- 2
 
 GRP2$FCthreshold <- 1
 GRP2$FDRthreshold <- 0.1
 
 
-Contrasts <- c("Glucose - Ethanol" = "Condition_Glucose - Condition_Ethanol")
-Contrasts <- c("GvsE" = "Condition_Glucose - Condition_Ethanol")
 
 
-#####
 
-datadir <- file.path(find.package("prolfquaData") , "quantdata")
-#inputMQfile <-  file.path(datadir, "MAXQuant_ComboCourse_p2691_March_2018_WU183012.zip")
-#inputAnnotation <- file.path(datadir, "annotation_ComboCourse_p2691_March_2018_WU183012.xlsx")
 
-inputMQfile <-  file.path(datadir, "MAXQuant_ComboCourse_p2370_March_2017_WU183008.zip")
-inputAnnotation <- file.path(datadir, "annotation_ComboCourse_p2370_March_2017_WU183008.xlsx")
+
+##### Read the data.
+
 startdata <- prolfqua::tidyMQ_ProteinGroups(inputMQfile)
+startdata$majProtID <- gsub(";.+","",startdata$majority.protein.ids)
 
 annotation <- readxl::read_xlsx(inputAnnotation)
-annotation$experiment = "p2691"
 
 
-
-
-##################################### ProteinID statistics
+##################################### ProteinID statistics #######
 
 startdata <- inner_join(annotation, startdata, by = "raw.file")
 startdata <- filter(startdata, nr.peptides >= GRP2$nrPeptides)
@@ -50,7 +46,6 @@ startdata <- startdata %>% mutate(proteinAnnot = case_when(grepl("^REV_",majorit
                                               grepl("^zz|^CON",majority.protein.ids) ~ "CON",
                                               TRUE ~ "FW"))
 
-startdata$majProtID <- gsub(";.+","",startdata$majority.protein.ids)
 distinctprotid <- startdata %>% select(protein_Id = majProtID, fasta.headers, proteinAnnot) %>% distinct()
 
 
@@ -60,14 +55,13 @@ GRP2$totalNrOfProteins <- sum(table(distinctprotid$proteinAnnot))
 GRP2$NrOfProteinsNoDecoys <- table(distinctprotid$proteinAnnot)["FW"]
 
 
-############################## Create configuration ####
+############################## Create configuration For MQ ####
 atable <- AnalysisTableAnnotation$new()
 atable$fileName = "raw.file"
 atable$hierarchy[["protein_Id"]] <- c("majProtID")
 atable$hierarchyDepth <- 1
 atable$setWorkIntensity("mq.protein.intensity")
-anaparam <- AnalysisParameters$new()
-config <- AnalysisConfiguration$new(atable, anaparam)
+config <- AnalysisConfiguration$new(atable)
 
 
 config$table$factors[[factorDisplayName]] = factorAnnotationName
@@ -109,18 +103,24 @@ GRP2$models <- mod
 GRP2$Contrast <- Contrasts
 
 contr <- prolfqua::Contrasts$new(mod, Contrasts)
-conrM <- ContrastsModerated$new(contr, modelName = "ModeratedLinear")
+conrM <- ContrastsModerated$new(contr, modelName = "Linear_Model_Moderated")
 mC <- ContrastsSimpleImpute$new(lfqdata = transformed, contrasts = Contrasts)
-conMI <- ContrastsModerated$new(mC, modelName = "ModeratedImpute")
+conMI <- ContrastsModerated$new(mC, modelName = "Imputed_Data")
 
 res <- prolfqua::addContrastResults(conrM, conMI)
 
 GRP2$contrResult <- res
-GRP2$contrMerged <- prolfqua::Contrasts_Plotter$new(res$merged, subject_Id = "protein_Id")
+GRP2$contrMerged <- prolfqua::Contrasts_Plotter$new(res$merged, subject_Id = "protein_Id",
+                                                    volcano = list(list(score = "FDR", fc = GRP2$FCthreshold,
+                                                                        thresh = GRP2$FDRthreshold)))
 GRP2$contrMore <- prolfqua::Contrasts_Plotter$new(res$more, subject_Id = "protein_Id")
 
+#GRP2$contrMerged$ma_plotly()
+
 tmp2 <- ungroup(GRP2$contrMerged$contrastDF)
-top20 <- tmp2 %>% dplyr::select( protein_Id,log2FC= estimate,conf.low,conf.high, FDR ) %>% arrange(FDR) %>% head(20)
+top20 <- tmp2 %>% dplyr::select( protein_Id,log2FC= estimate,conf.low,conf.high, FDR ) %>%
+  arrange(FDR) %>%
+  head(20)
 GRP2$top20 <- top20
 #knitr::kable(top20, caption = "Top 20 proteins sorted by smallest Q Value (adj.P.Val). The effectSize column is the log2 FC of condition vs reference.")
 
@@ -137,12 +137,12 @@ protMore$get_Plotter()$raster()
 GRP2$imputedProteins <- protMore
 
 # Plot proteins without p-values
+
 xx <- res$more[rowSums(is.na(res$more)) > 0,]
 if(nrow(xx) > 0){
   xx <- xx %>% arrange(estimate)
   GRP2$noPvalEstimate <- ggplot2::ggplot(xx ,aes(x = reorder(protein_Id, estimate), y = estimate)) +
     ggplot2::geom_bar(stat="identity") + coord_flip()
-
   missing <- GRP2$transformedlfqData$get_copy()
   missing$complete_cases()
   missing$data <- missing$data %>% dplyr::filter(protein_Id %in% xx$protein_Id)
@@ -150,8 +150,8 @@ if(nrow(xx) > 0){
 }
 
 
-
 ### -----
+
 
 rmarkdown::render("_GRP2Analysis.Rmd", params = list(grp = GRP2), output_format = bookdown::html_document2())
 
