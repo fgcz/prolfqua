c5954ddff48fd01ee5362a0fcd9b3d4ad8a8b4d2
wolski <wewolski@gmail.com>
2021-09-17 20:05:57 +0200

    rename

--- inst/run_scripts/run2GrpAnalysis_TMT.R
+++ inst/run_scripts/run2GrpAnalysis_TMT.R
@@ -1,47 +1,55 @@
+rm(list = ls())
 library(prolfqua)
 library(tidyverse)
 
+PG2a <- readxl::read_xlsx("data/Export_for_WW/o25914_Proteins..xlsx")
+res <- pivot_longer(PG2a, starts_with("Abundance"),names_to = "colname", values_to = "Abundance" )
+colnames(res) <- make.names(colnames(res))
+resA <- res %>% separate(colname, c(NA, "F", "channel", NA,"Condition"))
+resA <- rename(resA, nr.peptides = X..Peptides)
+
+
+
+annotation <- readxl::read_xlsx("TMT_lableling_o25914.xlsx")
+colnames(annotation) <- make.names(colnames(annotation))
+annotation <- annotation %>% mutate(channel = gsub("^TMT","", TMT.Tag)) %>% select(channel,Sample )
+annotation$channel[!annotation$channel %in% unique(resA$channel)]
+annotation <- annotation %>% separate(Sample, c(NA, "ID"), remove = FALSE)
+
+resAa <- inner_join(annotation, resA)
+resAa <- resAa %>% filter(!ID %in% c("SL1105","SL1665") )
 
 ################### CREATE SOME annotations
-datadir <- file.path(find.package("prolfquaData") , "quantdata")
-
-inputMQfile <-  file.path(datadir, "MAXQuant_ComboCourse_p2370_March_2017_WU183008.zip")
-inputAnnotation <- file.path(datadir, "annotation_ComboCourse_p2370_March_2017_WU183008.xlsx")
-
 GRP2 <- list()
 
 factorDisplayName <- "Condition_"
-factorAnnotationName <- "condition"
-Contrasts <- c("GvsE" = "Condition_Glucose - Condition_Ethanol")
+factorAnnotationName <- "Condition"
+Contrasts <- c("DiseasedvsControl" = "Condition_Diseased - Condition_Control")
 
-GRP2$projectID <- 3000
-GRP2$projectName <- "bbbbbbbbbbbbbbbbbbbb"
-GRP2$workunitID <- "PDrun"
+GRP2$projectID <- "25914"
+GRP2$projectName <- "Order_25914"
+GRP2$workunitID <- "Manual analysis"
 
 GRP2$nrPeptides <- 2
-
-GRP2$log2FCthreshold <- 1
+GRP2$log2FCthreshold <- 0.26
 GRP2$FDRthreshold <- 0.1
 
-
-##### Read the data.
-
-startdata <- prolfqua::tidyMQ_ProteinGroups(inputMQfile)
-startdata$majProtID <- gsub(";.+","",startdata$majority.protein.ids)
-annotation <- readxl::read_xlsx(inputAnnotation)
 
 
 ##################################### ProteinID statistics #######
 
-startdata <- inner_join(annotation, startdata, by = "raw.file")
+startdata <- resAa
 startdata <- filter(startdata, nr.peptides >= GRP2$nrPeptides)
-startdata <- startdata %>% mutate(proteinAnnot = case_when(grepl("^REV_",majority.protein.ids) ~ "REV",
-                                              grepl("^zz|^CON",majority.protein.ids) ~ "CON",
+startdata <- startdata %>% mutate(proteinAnnot = "FW")
+
+
+distinctprotid <- startdata %>% select(protein_Id = Accession, fasta.headers = Description) %>% distinct()
+
+distinctprotid <- distinctprotid %>% mutate(proteinAnnot = case_when(grepl("^REV_",protein_Id) ~ "REV",
+                                              grepl("^Y-FGCZ",protein_Id) ~ "CON",
                                               TRUE ~ "FW"))
 
-distinctprotid <- startdata %>% select(protein_Id = majProtID, fasta.headers, proteinAnnot) %>% distinct()
-desc <- distinctprotid %>% select(-proteinAnnot)
-
+distinctprotid$geneName <- sub(".* GN=(.+) PE.*", "\\1", distinctprotid$fasta.headers)
 GRP2$percentOfContaminants <-  table(distinctprotid$proteinAnnot)["CON"]/sum(table(distinctprotid$proteinAnnot)) * 100
 GRP2$percentOfFalsePositives <- table(distinctprotid$proteinAnnot)["REV"]/sum(table(distinctprotid$proteinAnnot)) * 100
 GRP2$totalNrOfProteins <- sum(table(distinctprotid$proteinAnnot))
@@ -50,25 +58,25 @@ GRP2$NrOfProteinsNoDecoys <- table(distinctprotid$proteinAnnot)["FW"]
 
 ############################## Create configuration For MQ ####
 atable <- AnalysisTableAnnotation$new()
-atable$fileName = "raw.file"
-atable$hierarchy[["protein_Id"]] <- c("majProtID")
+atable$fileName = "Sample"
+atable$hierarchy[["protein_Id"]] <- c("Accession")
 atable$hierarchyDepth <- 1
-atable$setWorkIntensity("mq.protein.intensity")
+atable$setWorkIntensity("Abundance")
 config <- AnalysisConfiguration$new(atable)
 
 
 config$table$factors[[factorDisplayName]] = factorAnnotationName
-config$table$factors[["run"]] = "Run_ID"
+config$table$factors[["ID"]] = "ID"
 config$table$factorDepth <- 1
 
+startdata$cha <- startdata$channel
 adata <- setup_analysis(startdata, config)
-adata$run <- as.numeric(adata$run)
 
 ##################### Preprocess intensities ###################################
 
 lfqdata <- LFQData$new(adata, config)
+lfqdata$data$Abundance <- lfqdata$data$Abundance * 1e6
 lfqdata$remove_small_intensities()
-lfqdata$factors()
 
 ### Do some type of data normalization (or do not)
 lt <- lfqdata$get_Transformer()
@@ -77,6 +85,7 @@ transformed <- lt$log2()$robscale()$lfq
 
 GRP2$lfqData <- lfqdata
 GRP2$transformedlfqData <- transformed
+#GRP2$transformedlfqData$data$transformedIntensity <- GRP2$transformedlfqData$data$transformedIntensity * 3
 
 ################## Run Modelling ###############
 
@@ -125,7 +134,6 @@ GRP2$top20confint <- ggplot(top20, aes(x = protein_Id, y = log2FC,
 protMore <- GRP2$transformedlfqData$get_copy()
 protMore$complete_cases()
 protMore$data <- protMore$data %>% filter(.data$protein_Id %in% res$more$contrast_result$protein_Id)
-
 GRP2$imputedProteins <- protMore
 
 # Plot proteins without p-values
@@ -144,13 +152,18 @@ if (nrow(xx) > 0) {
 
 ### -----
 
+names(GRP2)
 wr <- GRP2$lfqData$get_Writer()
 tmp <- wr$get_wide()
+tmp$data <- inner_join(distinctprotid, tmp$data)
+names(tmp)
 tmp2 <- GRP2$transformedlfqData$get_Writer()$get_wide()
+
 names(tmp2) <- paste0(names(tmp2), ".normalized")
-res <- inner_join(desc, GRP2$contrResult, by = c("protein_Id" = "protein_Id"))
-
+tmp2$data.normalized <- inner_join(distinctprotid, tmp2$data.normalized)
+res <- inner_join(distinctprotid, GRP2$contrResult)
 writexl::write_xlsx(c(tmp, tmp2,  contrasts = list(res)), path = "AnalysisResults.xlsx")
-
-rmarkdown::render("_GRP2Analysis.Rmd", params = list(grp = GRP2), output_format = bookdown::html_document2(toc = TRUE, toc_float = TRUE))
+rmarkdown::render("_GRP2Analysis.Rmd",
+                  params = list(grp = GRP2) ,
+                  output_format = bookdown::html_document2(toc = TRUE,toc_float = TRUE))
 
