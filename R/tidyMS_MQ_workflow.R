c9721df23ac4485399052982ab20814811b99b0f
wolski <wewolski@gmail.com>
2021-10-05 09:54:03 +0200

    Tiny changes

--- R/tidyMS_MQ_workflow.R
+++ R/tidyMS_MQ_workflow.R
@@ -65,3 +65,139 @@ filter_difference <- function(x, y, config){
     dplyr::anti_join(x, y, by = config$table$idVars())
   }
 }
+
+
+#' Create 2 grp report in html and write data to xlsx table
+#'
+#' For use examples see run_scripts directory
+#' @param startdata table in long format
+#' @param atable AnalysisTableAnnotation annotate startdata table
+#' @param GRP2 list with named arguments i.e. Contrasts, projectID, projectName, workunitID, nrPeptides, log2FCthreshold, FDRthreshold
+#' @param Accession column with protein accession
+#' @param Description column with portein desciription e.g. (fasta header)
+#' @param outapth directory to write results too.
+#' @export
+#'
+make2grpReport <- function(startdata, atable, GRP2, Accession = "Accession", Description = "Description", outpath = "."){
+
+  config <- prolfqua::AnalysisConfiguration$new(atable)
+
+  annotProtein <- function(startdata , Accession, Description, revpattern = "^REV_", contpattern = "^Y-FGCZ"){
+    GRP2 <- list()
+    distinctprotid <- startdata %>% select(protein_Id = {{Accession}}, fasta.headers = {{Description}}) %>% distinct()
+    distinctprotid <- distinctprotid %>% mutate(proteinAnnot = case_when(grepl(revpattern,protein_Id) ~ "REV",
+                                                                         grepl(contpattern,protein_Id) ~ "CON",
+                                                                         TRUE ~ "FW"))
+    GRP2$percentOfContaminants <-  round(mean(distinctprotid$proteinAnnot == "CON") * 100, digits = 2)
+    GRP2$percentOfFalsePositives <- round(mean(distinctprotid$proteinAnnot == "REV") * 100, digits = 2)
+    GRP2$totalNrOfProteins <- sum(table(distinctprotid$proteinAnnot))
+    GRP2$NrOfProteinsNoDecoys <- sum(distinctprotid$proteinAnnot == "FW")
+    return(list(stats = GRP2, distinctprotid = distinctprotid))
+  }
+
+  res <- annotProtein(startdata, !!sym(Accession), !!sym(Description))
+  GRP2 <- c(GRP2, res$stats)
+  distinctprotid <- res$distinctprotid
+  distinctprotid$geneName <- sub(".* GN=(.+) PE.*", "\\1", distinctprotid$fasta.headers)
+
+  ############################## Create configuration For MQ ####
+
+  adata <- setup_analysis(startdata, config)
+
+  ##################### Preprocess intensities ###################################
+
+  lfqdata <- LFQData$new(adata, config)
+  lfqdata$remove_small_intensities()
+
+  ### Do some type of data normalization (or do not)
+  lt <- lfqdata$get_Transformer()
+  transformed <- lt$log2()$robscale()$lfq
+
+
+  GRP2$lfqData <- lfqdata
+  GRP2$transformedlfqData <- transformed
+  #GRP2$transformedlfqData$data$transformedIntensity <- GRP2$transformedlfqData$data$transformedIntensity * 3
+
+  ################## Run Modelling ###############
+
+
+  formula_Condition <-  strategy_lm(paste0(transformed$config$table$getWorkIntensity(), " ~ ",
+                                           transformed$config$table$fkeysDepth()))
+  # specify model definition
+  modelName  <- "Model"
+
+  mod <- prolfqua::build_model(
+    transformed$data,
+    formula_Condition,
+    subject_Id = transformed$config$table$hierarchyKeys() )
+
+  GRP2$models <- mod
+
+
+
+  contr <- prolfqua::Contrasts$new(mod, GRP2$Contrasts)
+  conrM <- ContrastsModerated$new(contr, modelName = "Linear_Model_Moderated")
+  mC <- ContrastsSimpleImpute$new(lfqdata = transformed, contrasts = GRP2$Contrasts)
+  conMI <- ContrastsModerated$new(mC, modelName = "Imputed_Data")
+
+  res <- prolfqua::addContrastResults(conrM, conMI)
+
+  GRP2$contrResult <- res$merged$get_contrasts()
+  GRP2$contrMerged <- res$merged$get_Plotter()
+  GRP2$contrMerged$fcthresh = GRP2$log2FCthreshold
+  GRP2$contrMerged$volcano_spec[[1]]$thresh = GRP2$FDRthreshold
+
+  GRP2$contrMore <- res$more$get_Plotter()
+
+  top20 <- GRP2$contrResult %>% dplyr::select( protein_Id,log2FC= estimate,conf.low,conf.high, FDR ) %>%
+    arrange(FDR) %>%
+    head(20)
+  GRP2$top20 <- top20
+  #knitr::kable(top20, caption = "Top 20 proteins sorted by smallest Q Value (adj.P.Val). The effectSize column is the log2 FC of condition vs reference.")
+
+  GRP2$top20confint <- ggplot(top20, aes(x = protein_Id, y = log2FC,
+                                         ymin = conf.low, ymax = conf.high)) +
+    geom_hline( yintercept = 0, color = 'red' ) +
+    geom_linerange() + geom_point() + coord_flip() + theme_minimal()
+
+
+  protMore <- GRP2$transformedlfqData$get_copy()
+  protMore$complete_cases()
+  protMore$data <- protMore$data %>% filter(.data$protein_Id %in% res$more$contrast_result$protein_Id)
+  GRP2$imputedProteins <- protMore
+
+  # Plot proteins without p-values
+
+  xx <- res$more$contrast_result[rowSums(is.na(res$more$contrast_result)) > 0,]
+  if (nrow(xx) > 0) {
+    xx <- xx %>% arrange(estimate)
+    GRP2$noPvalEstimate <- ggplot2::ggplot(xx ,aes(x = reorder(protein_Id, estimate), y = estimate)) +
+      ggplot2::geom_bar(stat = "identity") + coord_flip()
+    missing <- GRP2$transformedlfqData$get_copy()
+    missing$complete_cases()
+    missing$data <- missing$data %>% dplyr::filter(protein_Id %in% xx$protein_Id)
+    missing$get_Plotter()$raster()
+  }
+
+
+  ### -----
+
+  wr <- GRP2$lfqData$get_Writer()
+  tmp <- wr$get_wide()
+  tmp$data <- inner_join(distinctprotid, tmp$data)
+  names(tmp)
+  tmp2 <- GRP2$transformedlfqData$get_Writer()$get_wide()
+
+  names(tmp2) <- paste0(names(tmp2), ".normalized")
+  tmp2$data.normalized <- inner_join(distinctprotid, tmp2$data.normalized)
+  res <- inner_join(distinctprotid, GRP2$contrResult)
+
+  writexl::write_xlsx(c(tmp, tmp2,  contrasts = list(res)),
+                      path = file.path(outpath,"AnalysisResults.xlsx"))
+
+  rmarkdown::render("_GRP2Analysis.Rmd",
+                    params = list(grp = GRP2) ,
+                    output_format = bookdown::html_document2(toc = TRUE,toc_float = TRUE))
+  file.copy("_GRP2Analysis.html", file.path(outpath, "_GRP2Analysis.html"), overwrite = TRUE)
+}
+
