04c309ec4965264543607dc7b91fee52f9f5d254
Witold Wolski <wewolski@gmail.com>
2021-11-03 10:50:31 +0100

    fix changes

--- R/tidyMS_R6_AnalysisConfiguration.R
+++ R/tidyMS_R6_AnalysisConfiguration.R
@@ -615,11 +615,12 @@ summarize_hierarchy <- function(pdata,
 #' x <- spreadValueVarsIsotopeLabel(analysis, conf)
 #' head(x[,5:ncol(x)])
 #'
+
 spreadValueVarsIsotopeLabel <- function(resData, config){
   table <- config$table
   idVars <- table$idVars()
   resData2 <- resData %>% dplyr::select(c(table$idVars(), table$valueVars()) )
-  resData2 <- resData2 %>% tidyr::gather(.data$variable, .data$value, -.data$idVars  )
+  resData2 <- resData2 %>% tidyr::gather(key = "variable", value = "value", -dplyr::all_of(idVars)  )
   resData2 <- resData2 %>%  tidyr::unite("temp", table$isotopeLabel, .data$variable )
   HLData <- resData2 %>% tidyr::spread(.data$temp,.data$value)
   invisible(HLData)
