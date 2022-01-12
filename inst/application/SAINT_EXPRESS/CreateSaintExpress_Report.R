f086a2c16d90d5f7b2b25c857f410e5b69604cda
Witold Wolski <wewolski@gmail.com>
2022-01-12 09:34:48 +0100

    adjust SaintExpress report to updates in read combined_protein function.

--- inst/application/SAINT_EXPRESS/CreateSaintExpress_Report.R
+++ inst/application/SAINT_EXPRESS/CreateSaintExpress_Report.R
@@ -1,8 +1,4 @@
-library(readr)
-library(tidyverse)
-library(prolfqua)
-
-
+# Read b-fabric related information
 yml <- yaml::read_yaml("config.yaml")
 
 BFABRIC <- list()
@@ -12,109 +8,110 @@ BFABRIC$projectID = yml$job_configuration$project_id
 BFABRIC$orderID = yml$job_configuration$order_id
 BFABRIC$inputID = yml$job_configuration$input[[1]][[1]]$resource_id
 BFABRIC$inputURL = yml$job_configuration$input[[1]][[1]]$resource_url
-
 BFABRIC$datasetID <- yml$application$parameters$datasetId
-
-
-spc <- if ( yml$application$parameters$SpcInt == "spc") { TRUE } else {FALSE}
-FCthreshold <- as.numeric( yml$application$parameters$FCthreshold )
-SSthreshold <- as.numeric( yml$application$parameters$SAINTscore )
-
 
 ZIPDIR = paste0("C",BFABRIC$projectID,"WU",BFABRIC$workunitID)
 dir.create(ZIPDIR)
 
-FDRthreshold <- 0.25
-FCthreshold <- 2
-SSthreshold <- 0.75
+
+# list with data used with the markdown report
+REPORTDATA <- list()
+
+# Applciation parameters
+REPORTDATA$spc <- if ( yml$application$parameters$SpcInt == "spc") { TRUE } else {FALSE}
+REPORTDATA$FCthreshold <- as.numeric( yml$application$parameters$FCthreshold )
+REPORTDATA$SSthreshold <- as.numeric( yml$application$parameters$SAINTscore )
+REPORTDATA$FDRthreshold <- 0.25
+
+# Prefix for exported files
 treat <- "FRAGPIPE_"
 
-
+# load data
 annotation <- readr::read_csv("dataset.csv")
 pp <- prolfqua::tidy_MSFragger_combined_protein_V16("combined_protein.tsv")
 
-
-
+# attach annotation to combined_protein data
 annotation$raw.file <- basename(annotation$`Relative Path`)
-annotation <- mutate(annotation, raw.file = paste0("x", gsub(".raw", "", tolower(raw.file))))
-annotation$raw.file %in% pp$raw.file
+annotation <- dplyr::mutate(annotation, raw.file = paste0("x", gsub(".raw", "", tolower(raw.file))))
 annotation$`Relative Path` <- NULL
-pdata <- inner_join(annotation, pp )
-pdata <- pdata %>% filter(combined.total.peptides > 1)
+stopifnot(sum(annotation$raw.file %in% pp$raw.file) > 0) # check that some files are annotated, if not exit script.
+pdata <- dplyr::inner_join(annotation, pp )
+
+# filter for more than 2 peptides per protein
+pdata <- pdata |> dplyr::filter(combined.total.peptides > 1)
+
+# configure prolfqua
 ata <- prolfqua::AnalysisTableAnnotation$new()
 ata$fileName = "raw.file"
-
 ata$factors[["CorT"]] = "CONTROL"
 ata$factors[["bait"]] = "BAIT"
 ata$factorDepth <- 2
 
 ata$hierarchy[["protein_Id"]] = "protein"
 
-if (spc) {
-    ata$workIntensity = "spectral.count"
+if (REPORTDATA$spc) {
+    ata$workIntensity = "razor.spectral.count"
 } else {
-    ata$workIntensity = "intensity"
+    ata$workIntensity = "razor.intensity"
 }
 
+
 config <- prolfqua::AnalysisConfiguration$new(ata)
-sdata <- setup_analysis(pdata, config)
-lfqdata <- LFQData$new(sdata, config)
+sdata <- prolfqua::setup_analysis(pdata, config)
+lfqdata <- prolfqua::LFQData$new(sdata, config)
 lfqdata$remove_small_intensities()
 
 
-# remove rev sequences
-lfqdata$data <- lfqdata$data %>% filter(!grepl("^REV__|^CON__", protein_Id))
-RESULTS <- list()
+# remove rev and contaminant sequences
+lfqdata$data <- lfqdata$data |> dplyr::filter(!grepl("^REV__|^CON__", protein_Id))
+
+
+RESULTS <- list() # RESULT is stored in excel table
 RESULTS$annotation <- lfqdata$factors()
 
-
+# Run Saint Analysis
 intdata <- lfqdata$data
-intdata <- inner_join(intdata , distinct( select(pdata, protein, protein.length)), by = c(protein_Id = "protein"))
+intdata <- dplyr::inner_join(intdata ,
+                      dplyr::distinct( dplyr::select(pdata, protein, protein.length)),
+                      by = c(protein_Id = "protein"))
 
-bb <- protein_2localSaint(intdata, quantcolumn = lfqdata$config$table$getWorkIntensity())
+bb <- prolfqua::protein_2localSaint(
+  intdata,
+  quantcolumn = lfqdata$config$table$getWorkIntensity())
 RESULTS <- c(RESULTS, bb)
-
-res <- runSaint(bb, spc = spc)
+res <- prolfqua::runSaint(bb, spc = REPORTDATA$spc)
 RESULTS <- c(RESULTS, res)
 
-###################
-ttt <- res$list
-cse <- ContrastsSaintExpress$new(ttt)
-pcse <- cse$get_Plotter(fcthreshold = log2(FCthreshold), bfdrthreshold = FDRthreshold)
-####################
-
+# write analysis results
 writexl::write_xlsx(RESULTS, path = file.path(ZIPDIR,paste0(treat, "_data.xlsx")))
 
-if (FALSE) {
-    sig <- cse$get_contrasts() %>%
-        filter(.data$BFDR  < FDRthreshold & .data$log2FC  >  log2(FCthreshold))
-} else {
-    sig <- cse$get_contrasts() %>%
-        filter(.data$SaintScore  >  SSthreshold & .data$log2FC  >  log2(FCthreshold))
-}
+# Prepare result visualization and render report
+cse <- prolfqua::ContrastsSaintExpress$new(res$list)
+pcse <- cse$get_Plotter(fcthreshold = log2(REPORTDATA$FCthreshold),
+                        bfdrthreshold = REPORTDATA$FDRthreshold)
+
+sig <- cse$get_contrasts() |>
+  dplyr::filter(.data$SaintScore  >  REPORTDATA$SSthreshold & .data$log2FC  >  log2(REPORTDATA$FCthreshold))
 
 tt <- lfqdata$get_Transformer()$log2()
-tt <- tt$robscale()
-lfqdata <- tt$lfq
+lfqdata <- tt$robscale()$lfq
 
-ReportData <- list()
-ReportData$BFABRIC <- BFABRIC
-ReportData$SSthreshold <- SSthreshold
-ReportData$FCthreshold <- FCthreshold
-ReportData$spc <- spc
-ReportData$lfqdata <- lfqdata
-ReportData$sig <- sig
-ReportData$cse <- cse
-ReportData$pcse <- pcse
+REPORTDATA$BFABRIC <- BFABRIC
+REPORTDATA$lfqdata <- lfqdata
+REPORTDATA$sig <- sig
+REPORTDATA$cse <- cse
+REPORTDATA$pcse <- pcse
 
 
-rm(list = setdiff(ls(), c("ReportData","ZIPDIR","treat")))
+rm(list = setdiff(ls(), c("REPORTDATA","ZIPDIR","treat"))) # delete all variables not needed for rendering
 
 rmarkdown::render("SaintExpressReportMsFragger.Rmd",
-                  params = list(sep = ReportData),
-                  output_format = bookdown::html_document2())
+                  params = list(sep = REPORTDATA),
+                  output_format = bookdown::html_document2(),
+                  envir = new.env())
 
 file.copy("SaintExpressReportMsFragger.html",
  file.path(ZIPDIR, paste0(treat, "SaintExpressReportMsFragger.html")),
  overwrite = TRUE)
+
 
