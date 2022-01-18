447581b1a64bf50d1dfcf5d553ca1b71128e49bb
Witold Wolski <wewolski@gmail.com>
2022-01-18 16:45:55 +0100

    moved _Grp2Analysis to inst/rmarkdown

--- R/vignetteHelpers.R
+++ R/vignetteHelpers.R
@@ -68,7 +68,7 @@ copy_mixed_model_analysis_script <- function(workdir = getwd()){
 #' @export
 #'
 copy_2grp_markdown <- function(workdir = getwd()){
-  runscripts <- c("run_scripts/_Grp2Analysis.Rmd","run_scripts/bibliography.bib")
+  runscripts <- c("rmarkdown/_Grp2Analysis.Rmd","run_scripts/bibliography.bib")
   .scriptCopyHelperVec(runscripts, workdir = workdir)
 }
 
