d479f948c713484a4fee1f153e6b1bb875343efa
Witold Wolski <wewolski@gmail.com>
2022-06-09 15:43:55 +0200

    new hex sticker

--- inst/Figures/hexsticker.R
+++ inst/Figures/hexsticker.R
@@ -18,3 +18,26 @@ sticker(imgurl,
 imgurl <- system.file("figures/cat.png", package="hexSticker")
 sticker(imgurl, package="hexSticker", p_size=20, s_x=1, s_y=.75, s_width=.6,
         filename="cat.png")
+
+
+library(hexSticker)
+imgurl = "canOpener.svg"
+sticker(imgurl,
+        package = "prolfqua",
+        p_color = "black",
+        p_size = 7,
+        p_y = 1.6,
+        s_x = 1,
+        s_y = 0.8,
+        s_width = 0.8,
+        h_fill = "green",
+        h_color = "darkgreen",
+        filename = "imgfile.png",
+        url	= "github.com/fgcz/prolfqua",
+        u_size = 2,
+        u_color = "black")
+
+
+#imgurl <- system.file("figures/cat.png", package="hexSticker")
+sticker(imgurl, package="hexSticker", p_size=20, s_x=1, s_y=.75, s_width=.6,
+        filename="cat.png")
