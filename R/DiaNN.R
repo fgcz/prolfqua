9df5389bd088ab6382062ad2afee9e470f55df33
Witold Wolski <wewolski@gmail.com>
2022-11-10 16:44:05 +0100

    diann read files.

--- R/DiaNN.R
+++ R/DiaNN.R
@@ -4,12 +4,12 @@
 #'
 #' @export
 #'
-read_diann_output <- function(path, nrPeptides = 2, Q.Value = 0.01){
+diann_read_output <- function(path, nrPeptides = 2, Q.Value = 0.01){
   add_nr_pep <- function(report){
     nrPEP <- report |>
-      dplyr::select(rlang::.data$Protein.Group, rlang::.data$Stripped.Sequence) |>
+      dplyr::select(all_of(c("Protein.Group", "Stripped.Sequence"))) |>
       dplyr::distinct() |>
-      dplyr::group_by(rlang::.data$Protein.Group) |>
+      dplyr::group_by(across("Protein.Group")) |>
       dplyr::summarize(nrPeptides = dplyr::n())
     report <- dplyr::inner_join(report, nrPEP)
     return(list(nrPEP = nrPEP, report = report))
@@ -18,7 +18,6 @@ read_diann_output <- function(path, nrPeptides = 2, Q.Value = 0.01){
   select_PG <- function(report){
     columns  <- c("File.Name",
                   "Protein.Group",
-                  #Protein.Ids,
                   "Protein.Names",
                   "PG.Quantity",
                   "PG.MaxLFQ",
@@ -28,21 +27,45 @@ read_diann_output <- function(path, nrPeptides = 2, Q.Value = 0.01){
                   "nrPeptides")
     columns %in% names(report)
 
-    PG <- report |> dplyr::select_at( columns
-    ) |> dplyr::distinct() |> dplyr::ungroup()
+    PG <- report |> dplyr::select( dplyr::all_of( columns )) |>
+      dplyr::distinct() |>
+      dplyr::ungroup()
     return(PG)
   }
 
   filter_PG <- function(PG, nrPeptides = 2, Q.Value = 0.01){
     PG <- PG |> dplyr::filter(nrPeptides >= nrPeptides)
-    PG <- PG |> dplyr::filter(rlang::.data$Lib.PG.Q.Value < Q.Value)
-    PG <- PG |> dplyr::filter(rlang::.data$PG.Q.Value < Q.Value)
+    PG <- PG |> dplyr::filter(.data$Lib.PG.Q.Value < Q.Value)
+    PG <- PG |> dplyr::filter(.data$PG.Q.Value < Q.Value)
   }
 
-  report <- read.table(path, sep = "\t", header = TRUE)
+  ## use internal functions
+  report <- readr::read_tsv(path)
   rNR <- add_nr_pep(report)
   PG <- select_PG(rNR$report)
   PG2 <- filter_PG(PG, nrPeptides = nrPeptides, Q.Value = Q.Value)
   report2 <- inner_join(PG2, report)
   return(report2)
+}
+
+#' Create peptide level (stripped sequences) report by aggregating Precursor abundances.
+#'
+#' \code{\link{diann_read_output}}
+#' @export
+#'
+diann_output_to_peptide <- function(report2){
+  peptide <- report2 |>
+  dplyr::group_by(across(c("raw.file",
+                           "Protein.Group",
+                           "Protein.Names",
+                           "PG.Quantity",
+                           "nrPeptides",
+                           "Stripped.Sequence" ) )) |>
+  dplyr::summarize(Peptide.Quantity = sum(.data$Precursor.Quantity, na.rm = TRUE),
+                   Peptide.Normalised = sum(.data$Precursor.Normalised, na.rm = TRUE),
+                   Peptide.Translated = sum(.data$Precursor.Translated, na.rm = TRUE),
+                   Peptide.Ms1.Translated = sum(.data$Ms1.Translated, na.rm = TRUE)
+                   ,.groups = "drop")
+  return(peptide)
+
 }
