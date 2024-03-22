606386f780af1155d1bcd275da60bb6d87dae8ec
Witold Wolski <wewolski@gmail.com>
2024-03-22 12:40:58 +0100

    see issue #58 moving files and not essential vignettes to new projekt proquatakeout

--- inst/plotly_reports/testingfacetwrap.R
+++
@@ -1,6 +0,0 @@
-protTable <- data.frame(
-  protein_Id = c("A","B","C"),
-  fc = c(2,3,-1),
-  p.value = c(0.1,0.01,0.4),
-  mean = c(3,10,20)
-  )
--- inst/win/lfq_MQ_SampleSizeReport.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-:: start help with "lfq_MQ_SampleSizeReport --help"
-
-SET args=%*
-  Rscript %~dp0/../run_scripts/lfq_MQ_SampleSizeReport.R %args%
-
-
