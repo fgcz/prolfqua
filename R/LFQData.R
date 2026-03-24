edc5dee30b06dc4d55c66f4daa4d61f05538bb14
Witold Wolski <wewolski@gmail.com>
2026-03-24 14:55:19 +0100

    Remove dead create_config_MQ_peptide, move filtering helpers to LFQData.R
    - Delete R/tidyMS_R6_ConcreteConfigurations.R (create_config_MQ_peptide
      had zero callers outside its own examples)
    - Move filter_proteins_by_peptide_count and filter_difference from
      R/tidyMS_MQ_workflow.R into R/LFQData.R as standalone exported functions
    - Delete R/tidyMS_MQ_workflow.R

--- R/LFQData.R
+++ R/LFQData.R
@@ -292,6 +292,66 @@ LFQData <- R6::R6Class(
   )
 )
 
+# Filtering helpers -----
+
+#' Keep only those proteins with 2 IDENTIFIED peptides
+#' @param pdata data.frame
+#' @param config AnalysisConfiguration
+#' @return list with data.frame (data) and name of new column (name)
+#' @export
+#' @keywords internal
+#' @family preprocessing
+#' @examples
+#'
+#' istar <- prolfqua::sim_lfq_data_peptide_config()
+#' filterPep <- prolfqua::filter_proteins_by_peptide_count( istar$data ,  istar$config )
+#' x <- prolfqua::summarize_hierarchy(filterPep$data , istar$config)
+#' stopifnot(x$peptide_Id_n >= istar$config$min_peptides_protein)
+#'
+filter_proteins_by_peptide_count <-
+  function(pdata, config) {
+    # remove single hit wonders
+    tmp <- prolfqua::nr_B_in_A(pdata, config)
+    if (!is.null(tmp)) {
+      res <- dplyr::filter(tmp$data, !!sym(tmp$name) >= config$min_peptides_protein)
+      name <- tmp$name
+    } else {
+      res <- pdata
+      name <- NULL
+    }
+    return(list(data = res, name = name))
+  }
+
+
+#' get the difference of two dataset where one is a subset of the other.
+#'
+#' @param x data.frame
+#' @param y data.frame
+#' @param config AnlysisConfiguration
+#' @return data.frame
+#' @export
+#' @keywords internal
+#'
+#' @examples
+#'
+#'
+#' istar <- prolfqua::sim_lfq_data_peptide_config()
+#' istar$config <- istar$config
+#' istar_data <- istar$data
+#' filterPep <- prolfqua:::filter_proteins_by_peptide_count( istar_data ,  istar$config )
+#' tmp <- filter_difference(istar_data, filterPep$data, istar$config)
+#' stopifnot(nrow(istar_data )  - nrow(filterPep$data) == nrow(tmp))
+#' tmp <- filter_difference(filterPep$data, istar_data , istar$config)
+#' stopifnot(nrow(istar_data )  - nrow(filterPep$data) == nrow(tmp))
+#'
+filter_difference <- function(x, y, config) {
+  if (nrow(y) > nrow(x)) {
+    dplyr::anti_join(y, x, by = config$id_vars())
+  } else {
+    dplyr::anti_join(x, y, by = config$id_vars())
+  }
+}
+
 
 #' converts LFQData object to SummarizedExperiment
 #'
